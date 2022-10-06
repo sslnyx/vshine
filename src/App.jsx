fd5119c1365c5cffc072e8b9686f46d4f436102a
Ning Yu Luo <sslnyx@gmail.com>
2022-10-06 10:27:52 -0700

    added bg video

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      {/* <video
+      <video
         className="absolute h-full w-full object-cover top-0 left-0"
         muted
         autoPlay
         loop
       >
         <source src="/videos/21_22_reel-m.mp4" type="video/mp4" />
-      </video> */}
+      </video>
 
       <div className="bg-black w-full h-full absolute top-0 left-0 bg-opacity-40"></div>
 
